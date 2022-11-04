eca14da1771fcaba2664a6f72bcef30bbf56bba3
Merzoit <68446392+Merzoit@users.noreply.github.com>
2022-11-04 14:54:04 +0300

    Rename

---
+++ 4_asyncio.py
@@ -0,0 +1,21 @@
+import asyncio
+import aiohttp
+
+
+def print_number():
+    number = 1
+    while True:
+        print(number)
+        number += 1
+
+
+def print_time():
+    pass
+
+
+def run():
+    pass
+
+
+if __name__ == '__main__':
+    run()
